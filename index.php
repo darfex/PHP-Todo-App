7f2d7eda01cd3b6d5ce8047457d09b63d57c7ccb
Dada Ifeoluwa <44666038+darfex@users.noreply.github.com>
2020-10-10 19:50:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require 'connection.php';
 <head>
     <title>My Todo</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
     <div class ="main-section">
@@ -80,8 +80,11 @@ require 'connection.php';
             <? endif; ?>
         </div>
     </div>
-
-<script src="js/jquery-3.5.1.min.js"></script>
+            
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
 <script>
 $(document).ready(function(){
     $(".check-box").click(function(){
